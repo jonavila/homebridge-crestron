74366a058fe90a0d3916e0ec5da3b04648338d0b
Jonathan Avila <jonathan@zoomdata.com>
2019-07-06 23:06:02 -0400

    Fix plugin name

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { Platform } from './Platform';
 import { Homebridge } from './@types';
 
 export default function(homebridge: Homebridge) {
-  homebridge.registerPlatform('homebridge-crestron', 'Crestron', Platform);
+  homebridge.registerPlatform('homebridge-crestron-simpl', 'Crestron', Platform);
 }
